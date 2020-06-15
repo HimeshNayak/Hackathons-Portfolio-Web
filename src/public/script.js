be6b3c9efb4a8eac209a6cf99b1d78809f4a9673
HimeshNayak <Himeshnayak015@gmail.com>
2020-06-16 01:22:26 +0530

    Added Firebase Backend By Harsh

--- src/public/script.js
+++ src/public/script.js
@@ -24,3 +24,52 @@ const navbar = document.querySelector(".navbar");
                 this.classList.add("active")
             })
         })
+
+		
+// ***************************************Adding to Firebase*******************************************************
+
+const fname= document.getElementById("fullName");
+const femail= document.getElementById("fullEmail");
+const msg= document.getElementById("mess");
+const abc= document.getElementById("butn");
+
+const jname= document.getElementById("joinName");
+const jemail= document.getElementById("joinEmail");
+const jgit= document.getElementById("joinGit");
+const jlink= document.getElementById("joinLink");
+const jskill= document.getElementById("joinSkill");
+const jexp= document.getElementById("joinExp");
+const jidea= document.getElementById("joinIdea");
+const defg = document.getElementById("joinbutn");
+
+//const database = firebase.database();
+const rootRef = database.ref("contactus");
+
+
+abc.addEventListener("click", (e) => {
+  e.preventDefault();
+  const autoId = rootRef.push().key
+  rootRef.child(autoId).set({
+    full_name: fname.value,
+    full_email: femail.value,
+    message: mess.value
+  })
+});
+
+const databasejoin = firebase.database();
+const rootRefjoin = databasejoin.ref("joinus");
+
+defg.addEventListener("click", (e) =>{
+  e.preventDefault();
+  const autoIdjoin= rootRefjoin.push().key
+   rootRefjoin.child(autoIdjoin).set({
+     join_name: jname.value,
+     join_email: jemail.value,
+     join_git: jgit.value,
+     join_linkedIn: jlink.value,
+     join_Skill: jskill.value,
+     join_experience: jexp.value,
+     join_idea: jidea.value,
+
+})
+});
