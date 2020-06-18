77b37f56edb9e1f93a62b53967596a7bfc6a2114
HimeshNayak <30944790+HimeshNayak@users.noreply.github.com>
2020-06-18 14:43:20 +0530

    Added information of all the members
    @harshgoyal7065 and @PSCoder10462 look into this and send this data accordingly to the portfolio page when respective member is clicked

--- src/public/script.js
+++ src/public/script.js
@@ -25,7 +25,106 @@ const navbar = document.querySelector(".navbar");
             })
         })
 
-		
+//Members Data
+const members = [
+    'Himesh Nayak',
+    'Haresh Nayak',
+    'Parv Sharma',
+    'Sarhtak Kh.',
+    'Kshitij Garg',
+    'Harsh Goyal',
+    'Rahul Gandhi',
+    'Shubham Rawal',
+    'Dhruv Pasricha',
+  ];
+
+  const membersPosition = [
+    'App Developer',
+    'ML Enthusiast',
+    'Web Developer',
+    'Web Developer',
+    'Web Developer',
+    'Web Developer',
+    'Web Developer',
+    'Web Developer',
+    'Web Developer'
+  ];
+
+  const imageMember = [
+    'himesh.jpg',
+    'haresh.jpg',
+    'parv.png',
+    'sarthak.png',
+    'kshitij.png',
+    'harsh.png',
+    'rahul.png',
+    'shubham.png',
+    'dhruv.jpg'
+  ];
+
+  const skillsHead = [
+    ['App Development','Full Stack Web Development', 'Programming languages', 'Others'],
+    ['App Development', 'Web Development', 'Programming Languages', 'Others',],
+    ['Frontend Web Development','Backend Web Development', 'Programming languages', 'Others'],
+    ['Frontend Web Development', 'Programming Languages'],
+    ['Frontend Web Development'],
+    ['Frontend Web Development', 'Backend Web Development', 'Programming Languages'],
+    ['Frontend Web Development','Backend Web Development', 'Programming languages', 'Others'],
+    ['Frontend Web Development', 'Programming languages'],
+    ['Frontend Web Development', 'Programming languages'],
+  ];
+
+  const skills = [
+    ['Android Studio, Java, Flutter', 'HTML, CSS, Javascript, NodeJs', 'C, C++, Python, Dart', 'Google Cloud'],
+    ['Flutter', 'HTML, CSS, Bootstrap', 'C, C++, Python, Dart, R', 'Machine Learning'],
+    ['HTML, CSS, Bootstrap', 'NodeJs, MongoDB, Javascript', 'C, C++', 'Semantic-UI'],
+    ['HTML, CSS', 'C++'],
+    ['HTML, CSS, Javasript'],
+    ['HTML, CSS, Bootstrap','Javascript, JQuery, NodeJs, Express', 'C, C++, Python'],
+    ['Android Studio', 'HTML', 'JavaScript', 'CSS'],
+    ['Machine Learning', 'Flutter', 'HTML', 'CSS'],
+    ['HTML', 'CSS','JAVAscript', 'NodeJs'],
+    ['HTML, CSS, Bootstrap', 'JavaScript, NodeJs, ExpressJs, MySQL', 'C, C++', 'ORM (Sequelize)'],
+    ['HTML, CSS', 'C, C++'],
+    ['HTML, CSS','C, C++'],
+  ];
+
+  const linkedin = [
+    'himeshnayak015',
+    'hareshnayak08',
+    'parv-sharma-3ba3991a1',
+    'sarthak-khandelwal-bb2825194',
+    'kshitij-garg-3b4229194',
+    'harsh-goyal-195255b9',
+    '',
+    'shubham-rawal-1195371a8',
+    'dhruvpasricha',
+  ];
+
+  const github = [
+    'HimeshNayak',
+    'hareshnayak',
+    'PSCoder10462',
+    'HellVolhard07',
+    'Kshitij-24',
+    'harshgoyal7065',
+    'rgtechno',
+    'shubham-rawal',
+    'dhruv-pasricha',
+  ];
+
+  const email = [
+    'himeshnayak015',
+    'sketchharry01',
+    'pscoder10462',
+    'itsmevasu84',
+    'shitijgarg24',
+    'harsh.student99',
+    'rahulg1701s',
+    'shubhamrawal2001',
+    'dpdhruv123',
+  ];
+
 // ***************************************Adding to Firebase*******************************************************
 
 const fname= document.getElementById("fullName");
